c5e11163d8c6ecccbc701b36419921db612cb0d8
Wenisclayton Mendes <wenisclayton.mendes@redemarajo.com.br>
2022-03-12 23:59:53 -0300

    Codigo Inicial

---
+++ RepositoryNet/Interface/IRepositoryAsync.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using RepositoryNet.Entities;
+
+namespace RepositoryNet.Interface
+{
+    public interface IRepositoryAsync<T> where T: EntityBase
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        IQueryable<T> GetAll(T entity);
+        Task<T> GetByIdAsync(Guid id);
+        Task UpdateAsync(T entity);
+        Task<T> GetFirstAsync(Expression<Func<T, bool>> expression);
+        IQueryable<T> Entities { get; }
+    }
+}
---
+++ RepositoryNet/RepositoryAsync.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using RepositoryNet.Entities;
+using RepositoryNet.Interface;
+
+namespace RepositoryNet
+{
+    public class RepositoryAsync<T> : IRepositoryAsync<T> where T : EntityBase
+    {
+        public async Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetFirstAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Entities { get; }
+    }
+}
